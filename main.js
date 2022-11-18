54b6cd1cad12f0d8b10f464f94f368c23d20f266
Mattia2119 <mattia.errol@gmail.com>
2022-11-18 09:51:00 +0100

    refactoring

--- main.js
+++ main.js
@@ -29,38 +29,17 @@ const keys = [
 
         generaDomande(key);
     
-        generaNumeri();
-    
         generaRows();
     
     }
 
     //Funzioni
     
-    //La funzione genera 4 numeri casuali compresi tra 0 e 7
-    //che saranno le nostre opzioni che l'utente potrà
-    //scegliere come risposta
-    function generaNumeri() {
-        let i = 0
-    
-        while(i < 4) {
-            let num = Math.floor(Math.random()*7);
-            if(!numeri_casuali.includes(num)) {
-                numeri_casuali.push(num);
-                i++;
-            } 
-        }
-    }
-    
-    function generaRows() {
-        for (let i = 0; i < cols.length; i++) {
-            cols[i].classList.remove('none');
-            for(let i = 0; i < cols.length; i++) {
-                cols[i].innerHTML = `<a href="#"><h1>${keys[0][numeri_casuali[i]]}</h1></a>`;
-            } 
-        }
-    }
-
+    //La funzione accetta un parametro in ingresso, che corrisponderà alla (key) tonalità scelta dall'utente
+    //aggiune la classe none al pannello di selezione della chiave e rende visibile la domanda
+    //genera venti numeri casuali compresi tra 1 e 7 e li pusha nell'array (interval) concatenandogli la stringa 'Maggiore'
+    //Dopodiché con la struttura di controllo (if/else if) farò in modo di far combaciare la chiave sceltà dall'utente
+    //con la domanda casuale relativa a quella specifica tonalità.
     function generaDomande(key) {
         start_panel.classList.add('none');
         question_panel.classList.remove('none');
@@ -70,7 +49,6 @@ const keys = [
         while(i < 20) {
             let rand = Math.floor(Math.random()*6 + 2);
             interval.push(rand + 'a Maggiore');
-            console.log(interval);
             i++
         }
 
@@ -90,6 +68,28 @@ const keys = [
             question_panel.innerHTML = `<h3>Qual'è la ${interval[0]} di ${keys[0][6]}?</h3>`;
         }       
 
+    }
+  
+    //La funzione genera 4 numeri casuali compresi tra 0 e 7
+    //che farò combaciare (nella funzione generaRows) 
+    //con la posizione nell'array keys della chiave selezionata dall'utente.
+    //scorre la lunghezza dell'array restituito dagli elementi cols nell'HTML,
+    //li rende visibili rimuovendo la classe none, e inietta l'html (a/h1) nel dom.
+    function generaRows() {
+        let i = 0
+    
+        while(i < 4) {
+            let num = Math.floor(Math.random()*7);
+            if(!numeri_casuali.includes(num)) {
+                numeri_casuali.push(num);
+                i++;
+            } 
+        }
+
+        for (let i = 0; i < cols.length; i++) {
+            cols[i].classList.remove('none');
+            cols[i].innerHTML = `<a href="#"><h1>${keys[0][numeri_casuali[i]]}</h1></a>`;
+        }
     }
 
     
